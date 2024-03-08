92fc3b0ffcbd4699e01d47358eda278db8f3a1ff
Xavi <xavi.quesada@iticbcn.cat>
2024-03-08 17:28:19 +0100

    WIP - templates

--- TIC_BCN_OR/centre/views.py
+++ TIC_BCN_OR/centre/views.py
@@ -4,10 +4,11 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('index_centre.html')
     return HttpResponse(template.render())
 
 def students(request):
+    # alumnes = [{"":"","":"","":"","":""},{"":""},{"":""},{"":""}]
     template = loader.get_template('students.html')
     return HttpResponse(template.render())
 
