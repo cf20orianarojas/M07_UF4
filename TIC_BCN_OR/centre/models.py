dd0d1a925c95cf2c05eb23ebe7b9821539360f31
cf20orianarojas <rojasoriana260217@gmail.com>
2024-03-22 16:32:29 +0100

    Modificat camp rol

--- TIC_BCN_OR/centre/models.py
+++ TIC_BCN_OR/centre/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # model usuari
 class User(models.Model):
+    options = [ ('professor', 'Professor'), ('alumne', 'Alumne')  ]
     nom = models.CharField(max_length = 20)
     cognom = models.CharField(max_length = 20)
     cognom2 = models.CharField(max_length = 20)
     email = models.CharField(max_length = 50)
-    rol = models.CharField(default='default', max_length = 20)
+    rol = models.CharField(max_length = 20, choices=options)
     curs = models.CharField(max_length = 20)
     moduls = models.CharField(max_length = 20)
